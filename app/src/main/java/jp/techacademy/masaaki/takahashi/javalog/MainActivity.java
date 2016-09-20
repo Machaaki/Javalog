d1901790352ceeb1f6e8665fd511cc82684318dd
machaaki <macha2346@gmail.com>
2016-09-20 17:00:26 +0900

    Java課題　Humanクラスの定義

--- app/src/main/java/jp/techacademy/masaaki/takahashi/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/masaaki/takahashi/javalog/MainActivity.java
@@ -11,16 +11,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Dog dog = new Dog("高橋", 32); //名前はポチ、年齢3歳で、Dogのインスタンスを作る
 
-        dog.say();
-        Log.d("javatest", "私の名前は" + dog.name + "です。");
-        Log.d("javatest", "年は" + dog.age + "歳です。");
 
-       Human2 human2 = new Human2("楽しい事"); // ~~を考えるのインスタンス
 
-        human2.hobby();
-        Log.d("Javatest", "私は" + human2.hobby + "考える");
+       Human human = new Human("ピアノ");
+
+        human.think();
+
+        human.say();
 
     }
 }
